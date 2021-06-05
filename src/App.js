0a3bf2e67c9e064481181fb95a34e0521d80853f
jaimeperezortega <jaimeperezortegadev@gmail.com>
2021-06-05 20:24:45 +0200

    Manual approah for filtering

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+
+  const [menuItems, setmenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  const filterItems = (category) => {
+    const itemsFiltered = items.filter(item => item.category === category); 
+    setmenuItems(itemsFiltered);
+  }
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories menuItems={menuItems} filterItems={filterItems}/>
+        <Menu items={menuItems}/>
+      </section>
+    </main>
+    )
 }
 
 export default App;
